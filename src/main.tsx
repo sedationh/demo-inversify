7ebc1439649dba1d7be8e0a636eb2ad723f27100
Holden Hu <sedationh@gmail.com>
2025-06-12 14:40:08 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,20 @@
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
+import { Container, inject } from "inversify"
 
 createRoot(document.getElementById("root")!).render(<App />)
+
+class Katana {
+  public readonly damage: number = 10
+}
+
+class Ninja {
+  constructor(@inject(Katana) public katana: Katana) {}
+}
+
+const container: Container = new Container()
+
+const ninja: Ninja = container.get(Ninja, { autobind: true })
+// const ninja: Ninja = container.get(Ninja)
+
+console.log(ninja.katana.damage)
